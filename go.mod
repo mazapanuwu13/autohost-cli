b42825dd2ca236109112f132c2a665baffb7446b
mazapanuwu13 <arturenriquez51@gmail.com>
2025-08-08 16:25:05 -0700

    Update go.mod and go.sum to include go-toml dependency; refactor main.go for improved error handling

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module autohost-cli
 go 1.23.0
 
 require (
+	github.com/pelletier/go-toml v1.9.5
+	github.com/spf13/cobra v1.9.1
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "autohost-cli/cmd"
+import (
+	"autohost-cli/cmd"
+	"autohost-cli/utils"
+	"log"
+)
 
 func main() {
+	err := utils.LoadURLsConfig("config/urls.toml")
+	if err != nil {
+		log.Fatalf("❌ No se pudo cargar config de URLs: %v", err)
+	}
 	cmd.Execute()
 }
